b3c3a44d096b836c033b4b7d03aad6d611ae7fb9
Elias Hanna <h.elias@hotmail.fr>
2018-11-20 13:32:59 +0100

    Changed the structure of the code to fit a E-Puck 2 robot

--- script/wall_follower.py
+++ script/wall_follower.py
@@ -6,12 +6,13 @@ import math
 from std_msgs.msg import Int16,Float32,Bool,Float32MultiArray,Int16MultiArray
 from nav_msgs.msg import Odometry
 from fastsim.srv import *
-from sensor_msgs.msg import LaserScan
 import tf
 
-lasers=LaserScan()
+# lasers=Float32MultiArray()
+lasers = list()
 odom = Odometry()
 
+# Rate for ROS in Hz
 rate = 10
 
 # Finish line variables
@@ -25,6 +26,10 @@ fl2_y = 300
 fl2_h = 50
 fl2_w = 10
 
+# Robot lasers range
+
+l_range = 100
+
 class Rect:
 	def __init__(self, x, y, h, w):
 		self._x = x;
@@ -35,7 +40,10 @@ class Rect:
 #-------------------------------------------
 def callback_lasers(data):
 	global lasers
-	lasers=data
+	lasers=list(data.data)
+	for i in range(0, len(lasers)):
+		if(lasers[i] == -1):
+			lasers[i] = l_range
 
 #-------------------------------------------
 def callback_odom(data):
@@ -104,7 +112,8 @@ def wall_follower():
 	pub_r = rospy.Publisher('/simu_fastsim/speed_right', Float32 , queue_size=10)
 
 	# If necessary, the node receives sensory information from simu_fastsim:
-	rospy.Subscriber("/simu_fastsim/laser_scan", LaserScan, callback_lasers)
+	# rospy.Subscriber("/simu_fastsim/laser_scan", LaserScan, callback_lasers)
+	rospy.Subscriber("/simu_fastsim/lasers", Float32MultiArray, callback_lasers)
 	rospy.Subscriber("/simu_fastsim/odom", Odometry, callback_odom)
 
 	# Targetted operating frequency of the node:
@@ -114,34 +123,40 @@ def wall_follower():
 	startT = rospy.get_time()
 	rospy.loginfo("Start time" + str(startT))
 
-	#Step number
+	# Step number
 	step = 0
 
 	fl1 = Rect(fl1_x, fl1_y, fl1_h, fl1_w)
 
 	fl2 = Rect(fl2_x, fl2_y, fl2_h, fl2_w)
 
+	# Initial desired angle
 	ang_des = math.radians(180)
 
+	# Proportionnal gain for P filter on orientation
 	Kp = 0.5
 
+	# Forward speed
 	u = 1
 
-	seuil = 35.0
+	# Threshold for distance detection 
+	seuil = 30.0
 
+	# Flag that triggers when the robot is rotating
 	rotating = False
+
 	# Main loop:
 	while (not rospy.is_shutdown()):
 		speed_l=0
 		speed_r=0
 
-		# Sum of values from the lasers segmented in 3 regions (left, right, front)
+		# Sum of values from the lasers segmented in 6 regions (front left, front right, front, rear left, rear right, rear)
 		sum_l = 0
 		sum_r = 0
 		sum_f = 0
 
 		# Number of lasers
-		sz = len(lasers.ranges)
+		sz = len(lasers)
 
 		# Processing of the finish line
 		#------------------------------------------------
@@ -151,34 +166,36 @@ def wall_follower():
 		# Processing of the sensory data :
 		#------------------------------------------------
 		if sz != 0 and rotating == False:
-			for i in range(0, sz/3):
-				sum_l += lasers.ranges[i]
-				sum_f += lasers.ranges[sz/3+i]
-				sum_r += lasers.ranges[2*sz/3+i]
+			for i in range(0,2):
+				sum_l += lasers[i + 1]
+				sum_r += lasers[i + 5]
+				if(i < 2):
+					sum_f += lasers[i + 3]
+			sum_f /= 2
+			sum_l /= 3
+			sum_r /= 3
 
-			sum_r /= sz/3
-			sum_f /= sz/3
-			sum_l /= sz/3
-			rospy.loginfo(str(sum_l) + " " + str(sum_f) + " " + str(sum_r))
+			# rospy.loginfo(str(sum_l) + " | " + str(sum_f) + " | " + str(sum_r))
 		
 			if(sum_r < seuil):			# Can't turn right
-				if(sum_f < seuil - 10):	# Can't go forward
+				if(sum_f < seuil - 5):	# Can't go forward
 					if(sum_l < seuil):	# Can't turn left
 						rospy.loginfo("I'm turning of 180 deg")
 						rotating = True
 						ang_des -= math.radians(180)
-					else:			# Can turn left
+					else:				# Can turn left
 						rospy.loginfo("I'm turning on my left")
 						rotating = True
 						ang_des -= math.radians(90)
-				else:				# Can go forward
+				else:					# Can go forward
 						ang_des += 0
-			elif(sum_f < seuil - 10):					# Can turn right
+			elif(sum_f < seuil - 5):	# Can turn right
 				rospy.loginfo("I'm turning on my right")
 				rotating = True
 				ang_des += math.radians(90)
 
-
+		# Processing of the orientation
+		#------------------------------------------------
 		quaternion = (
 		    odom.pose.pose.orientation.x,
 		    odom.pose.pose.orientation.y,
@@ -189,7 +206,9 @@ def wall_follower():
 		ang_des = toAbs(ang_des)
 
 		err = ang_des - ang_mes
-		print err
+
+		# Choice of the strategy : rotating or moving forward
+		#------------------------------------------------
 		if(abs(err) > 0.01):
 			cmd = Kp * err
 			if err > math.radians(180):
@@ -203,11 +222,6 @@ def wall_follower():
 			cmd = u
 			speed_l = cmd
 			speed_r = cmd
-		#if ang z < 0 partie sup
-		#if angz > 0 partie inf
-
-		rospy.loginfo("Vitesse actuelle: speed_l = " + str(speed_l) + " speed_r" + str(speed_r))
-		rospy.loginfo("ang_mes = " + str(ang_mes)+ " ang_des = " + str(ang_des) + " Rotating = " + str(rotating))
 
 		pub_l.publish(speed_l)
 		pub_r.publish(speed_r)
