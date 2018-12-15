2c7bf228953dbde0085551890833df1173582a5b
Elias Hanna <h.elias@hotmail.fr>
2018-12-15 21:12:18 +0100

    Reproducing experience of article

--- script/wall_follower.py
+++ script/wall_follower.py
@@ -17,7 +17,7 @@ rate = 10
 
 # Finish line variables
 fl1_x = 120
-fl1_y = 0
+fl1_y = 250
 fl1_h = 50
 fl1_w = 10
 
@@ -27,8 +27,16 @@ fl2_h = 50
 fl2_w = 10
 
 # Robot lasers range
-
 l_range = 100
+
+# Robot maximal command on each motor
+max_cmd = 0.8
+
+# Lap counter
+lap = 0
+
+# Time since last lap
+last_lap = 0
 
 class Rect:
 	def __init__(self, x, y, h, w):
@@ -51,57 +59,73 @@ def callback_odom(data):
 	odom=data
 
 #-------------------------------------------
-def toAbs(ang):
+def to_abs(ang):
 	if(ang < 0):
 		return 2*math.pi + ang
-	elif(ang > 2*math.pi):
+	elif(ang >= 2*math.pi):
 		return ang - 2*math.pi
 	else:
 		return ang
 
 #-------------------------------------------
-def isAtFinishLine(fl_nb, rect):
-	# 1) has the robot crossed the finish line ?
+def is_at_finish_line(fl_nb, rect):
+	global lap
+	# Has the robot crossed the finish line ?
 	rx = odom.pose.pose.position.x
 	ry = odom.pose.pose.position.y
 
-	if ((rx > rect._x and ry > rect._y) and (rx < rect._x + rect._w and ry < rect._y + rect._h)) and fl_nb == 1:
+	if ((rx > rect._x and ry > rect._y) and (rx < rect._x + rect._w and ry < rect._y + rect._h)) and fl_nb == 1 and lap == 3:
 		rospy.wait_for_service('simu_fastsim/teleport')
 		try:
 			# teleport robot
 			teleport = rospy.ServiceProxy('simu_fastsim/teleport', Teleport)
-			x  = fl2_x - 1
+			x  = fl2_x + fl2_w + 1
 			y  = 325
-			th = math.pi
+			th = 0
 			resp1 = teleport(x, y, th)
-			# store information about the duration of the finishing trial:
-			currT = rospy.get_time()
 
-			startT = currT
 			rx = x
 			ry = y
+
+			lap = 0
+
+			return True
 
 		except rospy.ServiceException, e:
 			print "Service call failed: %s"%e
 
-	elif ((rx > rect._x and ry > rect._y) and (rx < rect._x + rect._w and ry < rect._y + rect._h)) and fl_nb == 2:
+	elif ((rx > rect._x and ry > rect._y) and (rx < rect._x + rect._w and ry < rect._y + rect._h)) and fl_nb == 2 and lap == 3:
 		rospy.wait_for_service('simu_fastsim/teleport')
 		try:
 			# teleport robot
 			teleport = rospy.ServiceProxy('simu_fastsim/teleport', Teleport)
-			x  = fl1_x - 1
-			y  = 25
-			th = math.pi
+			x  = fl1_x + fl1_w + 1
+			y  = 280
+			th = 0
 			resp1 = teleport(x, y, th)
-			# store information about the duration of the finishing trial:
-			currT = rospy.get_time()
 
-			startT = currT
 			rx = x
 			ry = y
 
+			lap = 0
+
+			return True
+
 		except rospy.ServiceException, e:
 			print "Service call failed: %s"%e
+
+	return False
+
+#-------------------------------------------
+def lap_inc(rect):
+	global lap, last_lap
+	# Has the robot crossed a finish line ?
+	rx = odom.pose.pose.position.x
+	ry = odom.pose.pose.position.y
+
+	if ((rx > rect._x and ry > rect._y) and (rx < rect._x + rect._w and ry < rect._y + rect._h)) and (rospy.get_time() - last_lap) > 15:
+		lap += 1
+		last_lap = rospy.get_time()
 
 #-------------------------------------------
 def wall_follower():
@@ -111,7 +135,7 @@ def wall_follower():
 	pub_l = rospy.Publisher('/simu_fastsim/speed_left', Float32 , queue_size=10)
 	pub_r = rospy.Publisher('/simu_fastsim/speed_right', Float32 , queue_size=10)
 
-	# If necessary, the node receives sensory information from simu_fastsim:
+	# The node receives sensory information from simu_fastsim:
 	# rospy.Subscriber("/simu_fastsim/laser_scan", LaserScan, callback_lasers)
 	rospy.Subscriber("/simu_fastsim/lasers", Float32MultiArray, callback_lasers)
 	rospy.Subscriber("/simu_fastsim/odom", Odometry, callback_odom)
@@ -121,7 +145,7 @@ def wall_follower():
 	
 	# start time and timing related things
 	startT = rospy.get_time()
-	rospy.loginfo("Start time" + str(startT))
+	rospy.loginfo("Start time: " + str(startT))
 
 	# Step number
 	step = 0
@@ -131,13 +155,10 @@ def wall_follower():
 	fl2 = Rect(fl2_x, fl2_y, fl2_h, fl2_w)
 
 	# Initial desired angle
-	ang_des = math.radians(180)
+	ang_des = math.radians(0)
 
 	# Proportionnal gain for P filter on orientation
-	Kp = 0.5
-
-	# Forward speed
-	u = 1
+	Kp = 0.8
 
 	# Threshold for distance detection 
 	seuil = 30.0
@@ -158,24 +179,34 @@ def wall_follower():
 		# Number of lasers
 		sz = len(lasers)
 
+		# Processing of the lap counter
+		#------------------------------------------------
+		lap_inc(fl1)
+		lap_inc(fl2)
+		# rospy.loginfo("Lap number: " + str(lap+1))
+
 		# Processing of the finish line
 		#------------------------------------------------
-		isAtFinishLine(1, fl1);
-		isAtFinishLine(2, fl2);
+		if(is_at_finish_line(1, fl1)):
+			ang_des = math.radians(0)
+		if(is_at_finish_line(2, fl2)):
+			ang_des = math.radians(0)
 		
 		# Processing of the sensory data :
 		#------------------------------------------------
 		if sz != 0 and rotating == False:
-			for i in range(0,2):
-				sum_l += lasers[i + 1]
-				sum_r += lasers[i + 5]
-				if(i < 2):
-					sum_f += lasers[i + 3]
+			for i in range(1,3):
+				sum_l += lasers[i]
+				sum_r += lasers[i + 4]
+				sum_f += lasers[i + 2]
 			sum_f /= 2
-			sum_l /= 3
-			sum_r /= 3
+			sum_l /= 2
+			sum_r /= 2
 
-			# rospy.loginfo(str(sum_l) + " | " + str(sum_f) + " | " + str(sum_r))
+			# rospy.loginfo("Raw lasers values: " + str(lasers[0]) + " | " + str(lasers[1]) + " | " + str(lasers[2]) + " | " + str(lasers[3]) \
+			# 		+ " | " + str(lasers[4]) + " | " + str(lasers[5]) + " | " + str(lasers[6]) + " | " + str(lasers[7]))
+
+			# rospy.loginfo("Sum in regions: " + str(sum_l) + " | " + str(sum_f) + " | " + str(sum_r))
 		
 			if(sum_r < seuil):			# Can't turn right
 				if(sum_f < seuil - 5):	# Can't go forward
@@ -202,15 +233,23 @@ def wall_follower():
 		    odom.pose.pose.orientation.z,
 		    odom.pose.pose.orientation.w)
 		ang_mes = tf.transformations.euler_from_quaternion(quaternion)[2]
-		ang_mes = toAbs(ang_mes)
-		ang_des = toAbs(ang_des)
+		rospy.loginfo("ang_mes avant: " + str(ang_mes))
+		ang_mes = to_abs(ang_mes)
+		ang_des = to_abs(ang_des)
+
+		rospy.loginfo("ang_des: " + str(ang_des) + " ang_mes: " + str(ang_mes))
 
 		err = ang_des - ang_mes
+
+		# Case where the robot has to turn in anti-trigo way but goes the other
+		if err < -math.radians(180):
+			err += 2*math.pi
 
 		# Choice of the strategy : rotating or moving forward
 		#------------------------------------------------
 		if(abs(err) > 0.01):
 			cmd = Kp * err
+			cmd = (cmd if (cmd<max_cmd) else max_cmd) if (cmd>0) else (cmd if (cmd>-max_cmd) else -max_cmd)
 			if err > math.radians(180):
 				speed_l = cmd
 				speed_r = -cmd
@@ -219,7 +258,7 @@ def wall_follower():
 				speed_r = cmd
 		else:
 			rotating = False
-			cmd = u
+			cmd = max_cmd
 			speed_l = cmd
 			speed_r = cmd
 
